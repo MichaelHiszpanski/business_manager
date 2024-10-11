049e6fac13942dd5b8d503ada9363829036562c7
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-11 09:38:40 +0100

    observer

---
+++ lib/core/tools/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const CHECK_NETWORK_STATUS_URL = "google.com";
+  static const CHECK_NETWORK_STATUS_TIMEOUT_IN_SECONDS = 5;
+  static const MOBILE_WIDTH = 600;
+
+  static const MAX_LENGHT_TEXT_TITLE = 20;
+  static const MAX_LENGHT_TEXT_CONTENT = 300;
+}
---
+++ lib/core/tools/flutter_helper.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:dissertation_project_app/core/tools/constants.dart';
+
+extension BuildContextExtension on BuildContext {
+  double get deviceHeight => MediaQuery.of(this).size.height;
+
+  double get deviceWidth => MediaQuery.of(this).size.width;
+
+  // AppLocalizations get strings => AppLocalizations.of(this);
+
+  bool get isMobile => MediaQuery.of(this).size.width < Constants.MOBILE_WIDTH;
+
+  bool get isTablet => MediaQuery.of(this).size.width >= Constants.MOBILE_WIDTH;
+
+  double get workspaceHeight {
+    final screenHeight = MediaQuery.of(this).size.height;
+    final basicAppBarHeight = MediaQuery.of(this).padding.top + kToolbarHeight;
+    final basicBottomBarHeight =
+        MediaQuery.of(this).padding.bottom + kBottomNavigationBarHeight;
+    return screenHeight - basicAppBarHeight - basicBottomBarHeight;
+  }
+
+  void navigateBackSafe() {
+    if (Navigator.of(this).canPop()) Navigator.of(this).pop();
+  }
+}
