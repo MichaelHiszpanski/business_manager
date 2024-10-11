de799255b1fe747c0e29e51753e6bc4d4ddb9bcb
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-11 09:38:01 +0100

    main block

---
+++ lib/core/main_utils/bloc_provider/bloc_provider.dart
@@ -0,0 +1,24 @@
+import 'package:business_manager/core/main_utils/main_bloc/main_bloc.dart';
+import 'package:business_manager/feature/services/to_do_list/bloc/to_do_bloc.dart';
+import 'package:business_manager/feature/services/work_manager/bloc/work_manager_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocProviders extends StatelessWidget {
+  final Widget child;
+
+  const AppBlocProviders({required this.child, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => MainBloc()),
+        //..
+        BlocProvider(create: (context) => ToDoBloc()),
+        BlocProvider(create: (context) => WorkManagerBloc()),
+      ],
+      child: child,
+    );
+  }
+}
