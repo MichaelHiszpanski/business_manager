8a8518bf555bceaa707617383f0d79d73a10e174
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-11 09:38:17 +0100

    routes and repo

---
+++ lib/core/main_utils/app_routes/app_routes.dart
@@ -0,0 +1,38 @@
+import 'package:business_manager/core/screens/load_app_data_screen.dart';
+import 'package:business_manager/core/screens/test_screen.dart';
+import 'package:business_manager/feature/services/to_do_list/presentation/screens/add_item_page.dart';
+import 'package:business_manager/feature/services/to_do_list/presentation/screens/to_do_page.dart';
+import 'package:business_manager/feature/services/work_manager/presentation/screens/add_new_event_screen.dart';
+import 'package:business_manager/feature/services/work_manager/presentation/screens/work_manager_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:business_manager/home_page.dart';
+
+class AppRoutes {
+  static const String homePage = '/home_page';
+  static const String toDoPage =
+      '/services/to_do_list/presentation/screens/to_do_page';
+  static const String addItemPage =
+      '/services/to_do_list/presentation/screens/add_item_page';
+  static const String weatherPage = '/weather_app/weather_page';
+  static const String loadAppDataScreen = '/core/screens/load_app_data_screen';
+  static const String testScreen = '/core/screens/test_screen';
+
+  //WORK-MANAGER
+  static const String workManagerScreen =
+      '/services/work_manager/presentation/work_manager_screen';
+
+  static const String addNewEventScreen =
+      '/services/work_manager/presentation/add_new_event_screen';
+
+  static final routes = <String, WidgetBuilder>{
+    homePage: (context) => const HomePage(title: "Home Page"),
+    toDoPage: (context) => const ToDoPage(),
+    addItemPage: (context) => AddItemPage(),
+    loadAppDataScreen: (context) => const LoadAppDataScreen(),
+    testScreen: (context) => RotatingCirclesScreen(),
+    workManagerScreen: (context) => const WorkManagerScreen(),
+    addNewEventScreen: (context) => const AddNewEventScreen(
+          isLastDateVisible: true,
+        )
+  };
+}
